3f4ccdc2522bbcd90fcbf6cab0984ac051de5811
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2025-02-12 13:46:18 +0800

    Update index.tsx

--- packages/scan/src/web/views/toolbar/index.tsx
+++ packages/scan/src/web/views/toolbar/index.tsx
@@ -1,5 +1,6 @@
 // @TODO: @pivanov - finish the pin functionality
-import { useCallback, useEffect, useRef } from 'preact/hooks';
+import { useSignalEffect } from '@preact/signals';
+import { useCallback } from 'preact/hooks';
 import {
   type LocalStorageOptions,
   ReactScanInternals,
@@ -7,13 +8,12 @@ import {
 } from '~core/index';
 import { Icon } from '~web/components/icon';
 import { Toggle } from '~web/components/toggle';
-import { signalIsSettingsOpen } from '~web/state';
 import { cn, readLocalStorage, saveLocalStorage } from '~web/utils/helpers';
 import { constant } from '~web/utils/preact/constant';
 import FpsMeter from '~web/widget/fps-meter';
 
 export const Toolbar = constant(() => {
-  const refSettingsButton = useRef<HTMLButtonElement>(null);
+  // const refSettingsButton = useRef<HTMLButtonElement>(null);
   // const [isPinned, setIsPinned] = useState(false);
   // const [metadata, setMetadata] = useState<FiberMetadata | null>(null);
 
@@ -68,34 +68,35 @@ export const Toolbar = constant(() => {
   //   signalIsSettingsOpen.value = !signalIsSettingsOpen.value;
   // }, []);
 
-  useEffect(() => {
-    const unSubState = Store.inspectState.subscribe((state) => {
-      if (state.kind === 'uninitialized') {
-        Store.inspectState.value = {
-          kind: 'inspect-off',
-        };
-      }
+  useSignalEffect(() => {
+    const state = Store.inspectState.value;
+    if (state.kind === 'uninitialized') {
+      Store.inspectState.value = {
+        kind: 'inspect-off',
+      };
+    }
 
-      // if (state.kind === 'focused' && state.fiber) {
-      //   const pinned = readLocalStorage<FiberMetadata>('react-scann-pinned');
-      //   setIsPinned(!!pinned);
+    // if (state.kind === 'focused' && state.fiber) {
+    //   const pinned = readLocalStorage<FiberMetadata>('react-scann-pinned');
+    //   setIsPinned(!!pinned);
 
-      //   const m = getFiberMetadata(state.fiber);
-      //   if (m !== null) {
-      //     setMetadata(m);
-      //   }
-      // }
-    });
+    //   const m = getFiberMetadata(state.fiber);
+    //   if (m !== null) {
+    //     setMetadata(m);
+    //   }
+    // }
+  });
 
-    const unSubSettings = signalIsSettingsOpen.subscribe((state) => {
-      refSettingsButton.current?.classList.toggle('text-inspect', state);
-    });
+  // useEffect(() => {
 
-    return () => {
-      unSubState();
-      unSubSettings();
-    };
-  }, []);
+  //   const unSubSettings = signalIsSettingsOpen.subscribe((state) => {
+  //     refSettingsButton.current?.classList.toggle('text-inspect', state);
+  //   });
+
+  //   return () => {
+  //     unSubSettings();
+  //   };
+  // }, []);
 
   // const onTogglePin = useCallback(() => {
   //   if (isPinned) {
@@ -122,9 +123,7 @@ export const Toolbar = constant(() => {
   }
 
   return (
-    <div
-      className="flex max-h-9 min-h-9 flex-1 items-stretch overflow-hidden gap-x-[6px]"
-    >
+    <div className="flex max-h-9 min-h-9 flex-1 items-stretch overflow-hidden gap-x-[6px]">
       <div className="h-full flex items-center min-w-fit gap-x-[6px]">
         <button
           type="button"
