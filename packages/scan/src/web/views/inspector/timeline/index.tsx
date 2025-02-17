1ddf39e231fd1ed3d2818a51a569c3d324862c06
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2025-02-17 23:51:48 +0800

    Refactor `Timeline`

--- packages/scan/src/web/views/inspector/timeline/index.tsx
+++ packages/scan/src/web/views/inspector/timeline/index.tsx
@@ -1,43 +1,39 @@
+import { computed, useComputed } from '@preact/signals';
 import { isInstrumentationActive } from 'bippy';
 import { memo } from 'preact/compat';
-import { useCallback, useEffect, useMemo, useRef } from 'preact/hooks';
+import { useCallback } from 'preact/hooks';
 import { Icon } from '~web/components/icon';
 import { Slider } from '~web/components/slider';
 import type { useMergedRefs } from '~web/hooks/use-merged-refs';
-import {
-  timelineActions,
-  timelineState,
-} from '../states';
+import { timelineActions, timelineState } from '../states';
 import { calculateSliderValues } from '../utils';
 
 interface TimelineProps {
   refSticky?:
-  | ReturnType<typeof useMergedRefs<HTMLElement>>
-  | ((node: HTMLElement | null) => void);
+    | ReturnType<typeof useMergedRefs<HTMLElement>>
+    | ((node: HTMLElement | null) => void);
 }
 
-export const Timeline = memo(({
-  refSticky,
-}: TimelineProps) => {
-  const refPlayInterval = useRef<number | null>(null);
+const buttonTitle = computed(() =>
+  timelineState.value.isVisible
+    ? 'Hide Re-renders History'
+    : 'View Re-renders History',
+);
 
-  const {
-    currentIndex,
-    isVisible,
-    totalUpdates,
-    updates,
-  } = timelineState.value;
-
-  const sliderValues = useMemo(() => {
-    return calculateSliderValues(totalUpdates, currentIndex);
-  }, [totalUpdates, currentIndex]);
+export const Timeline = memo(({ refSticky }: TimelineProps) => {
+  const sliderValues = useComputed(() => {
+    return calculateSliderValues(
+      timelineState.value.totalUpdates,
+      timelineState.value.currentIndex,
+    );
+  });
 
   const handleSliderChange = async (e: Event) => {
+    const { updates } = timelineState.value;
     const target = e.target as HTMLInputElement;
     const value = Number.parseInt(target.value, 10);
 
     const newIndex = Math.min(updates.length - 1, Math.max(0, value));
-
 
     let isViewingHistory = false;
     if (newIndex > 0 && newIndex < updates.length - 1) {
@@ -46,35 +42,25 @@ export const Timeline = memo(({
     timelineActions.updateFrame(newIndex, isViewingHistory);
   };
 
-  useEffect(() => {
-    return () => {
-      if (refPlayInterval.current) {
-        clearInterval(refPlayInterval.current);
-      }
-    };
-  }, []);
-
-  const handleShowTimeline = useCallback(() => {
-    if (!isVisible) {
+  const handleShowTimeline = () => {
+    if (!timelineState.value.isVisible) {
       timelineActions.showTimeline();
     }
-  }, [isVisible]);
+  };
 
   const handleHideTimeline = useCallback((e: Event) => {
-    e.preventDefault();
-    e.stopPropagation();
-    if (refPlayInterval.current) {
-      clearInterval(refPlayInterval.current);
-      refPlayInterval.current = null;
+    if (timelineState.value.isVisible) {
+      e.preventDefault();
+      e.stopPropagation();
+      timelineActions.hideTimeline();
     }
-    timelineActions.hideTimeline();
   }, []);
 
   if (!isInstrumentationActive()) {
     return null;
   }
 
-  if (totalUpdates <= 1) {
+  if (timelineState.value.totalUpdates <= 1) {
     return null;
   }
 
@@ -88,34 +74,35 @@ export const Timeline = memo(({
     >
       <button
         type="button"
-        onClick={isVisible ? handleHideTimeline : undefined}
-        title={isVisible ? 'Hide Re-renders History' : 'View Re-renders History'}
+        onClick={handleHideTimeline}
+        title={buttonTitle}
         className="w-4 h-4 flex items-center justify-center"
       >
         <Icon name="icon-gallery-horizontal-end" size={12} />
       </button>
-      {
-        isVisible
-          ? (
-            <>
-              <div className="text-xs text-gray-500">
-                {sliderValues.leftValue}
-              </div>
-              <Slider
-                min={sliderValues.min}
-                max={sliderValues.max}
-                value={sliderValues.value}
-                onChange={handleSliderChange}
-                className="flex-1"
-                totalUpdates={sliderValues.rightValue + 1}
-              />
-              <div className="text-xs text-gray-500">
-                {sliderValues.rightValue}
-              </div>
-            </>
-          )
-          : 'View Re-renders History'
-      }
+      {useComputed(() =>
+        timelineState.value.isVisible ? (
+          // TODO granular pls
+          <>
+            <div className="text-xs text-gray-500">
+              {sliderValues.value.leftValue}
+            </div>
+            <Slider
+              min={sliderValues.value.min}
+              max={sliderValues.value.max}
+              value={sliderValues.value.value}
+              onChange={handleSliderChange}
+              className="flex-1"
+              totalUpdates={sliderValues.value.rightValue + 1}
+            />
+            <div className="text-xs text-gray-500">
+              {sliderValues.value.rightValue}
+            </div>
+          </>
+        ) : (
+          'View Re-renders History'
+        ),
+      )}
     </button>
   );
 });
