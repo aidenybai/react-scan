81071c5f110d6200dab83ff377423d4360f04e78
wanho1108 <wanho1108@gmail.com>
2026-01-04 18:06:30 +0900

    fix(scan): add `postcss-nesting` for older Chromium support

--- packages/scan/postcss.config.mjs
+++ packages/scan/postcss.config.mjs
@@ -1,7 +1,13 @@
 import autoprefixer from 'autoprefixer';
+import postcssNesting from 'postcss-nesting';
 import tailwindcss from 'tailwindcss';
 import remToPx from './postcss.rem2px.mjs';
 
 export default {
-  plugins: [remToPx({ baseValue: 16 }), tailwindcss, autoprefixer],
+  plugins: [
+    remToPx({ baseValue: 16 }),
+    postcssNesting,
+    tailwindcss,
+    autoprefixer,
+  ],
 };
